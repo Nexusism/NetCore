7ec8da1370dae7446bbaaf676238847c111d6144
DESKTOP-T2J33CS\LJG <wanggoog@naver.com>
2023-04-03 21:28:06 +0900

    의존성주입 - Service를 직접 생성하는 방식

---
+++ NetCore.Services/Svcs/UserService.cs
@@ -0,0 +1,40 @@
+﻿using NetCore.Data.DataModels;
+using NetCore.Data.ViewModels;
+using NetCore.Services.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore.Services.Svcs
+{
+    public class UserService : IUser
+    {
+        #region private methods
+        private IEnumerable<User> GetUserInfos()
+        {
+            return new List<User>()
+            {
+                new User()
+                {
+                    UserId = "ffpower",
+                    UserName = "이종국",
+                    UserEmail = "wanggoog@naver.com",
+                    Password = "123123"
+                }
+            };
+        }
+
+        private bool checkTheUserInfo(string userId, string password)
+        {
+            return GetUserInfos().Where(u => u.UserId.Equals(userId) && u.Password.Equals(password)).Any();
+        }
+        #endregion
+
+        bool IUser.MatchTheUserInfo(LoginInfo login)
+        {
+            return checkTheUserInfo(login.UserId, login.Password);
+        }
+    }
+}
--- NetCore.Web/Controllers/MemberShipController.cs
+++ NetCore.Web/Controllers/MemberShipController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NetCore.Data.ViewModels;
+using NetCore.Services.interfaces;
+using NetCore.Services.Svcs;
 using NetCore.Web.Models;
 
 namespace NetCore.Web.Controllers
 {
     public class MemberShipController : Controller
     {
+        // 의존성 주입 - 생성자 주입(닷넷코어 기본제공)
+        private IUser _user;
         public IActionResult Index()
         {
             return View();
@@ -18,20 +24,37 @@ namespace NetCore.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        // Services => Web
+        // Services 
         public IActionResult Login(LoginInfo login)
         {
             string message = string.Empty;
 
             if (ModelState.IsValid)
             {
-                string userId = "ffpower";
-                string password = "123456";
+                // 뷰모델
+                // 서비스 개념
+
+                //if (login.UserId.Equals(userId) && login.Password.Equals(password))
+                if (_user.MatchTheUserInfo(login))
+
+                {
+                    TempData["Message"] = "로그인이 성공적으로 이루어졌습니다.";
+
+                    return RedirectToAction("Index", "MemberShip");
+                }
+                else
+                {
+                    message = "로그인되지 않았습니다.";
+                }
             }
             else
             {
                 message = "로그인 정보를 올바르게 입력하세요.";
 
             }
+
+            ModelState.AddModelError(string.Empty, message);
             return View(login);
         }
 
