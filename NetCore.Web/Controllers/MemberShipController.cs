d6cc2b952a63c85050227f3c2aedb3c92bbe894f
Nexusism <wanggoog91@gmail.com>
2023-04-03 16:56:59 +0900

    ASP.NET Core MVC 1일차-1

---
+++ NetCore.Web/Controllers/MemberShipController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.Web.Models;
+
+namespace NetCore.Web.Controllers
+{
+    public class MemberShipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginInfo login)
+        {
+            string message = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+                string userId = "ffpower";
+                string password = "123456";
+            }
+            else
+            {
+                message = "로그인 정보를 올바르게 입력하세요.";
+
+            }
+            return View(login);
+        }
+
+    }
+}
---
+++ NetCore.Web/Models/LoginInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCore.Web.Models
+{
+    public class LoginInfo
+    {
+        
+        [Required(ErrorMessage = "사용자 ID를 입력하세요.")]
+        [MinLength(6, ErrorMessage = "사용자 ID는 최소 6자 이상 입력하세요.")]
+        [Display(Name = "사용자ID")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "비밀번호를 입력하세요.")]
+        [MinLength(6, ErrorMessage = "비밀번호는 최소 6자 이상 입력하세요.")]
+        [Display(Name = "비밀번호")]
+        public string Password { get; set; }
+    }
+}
